5562d6c68228c0fb1ff0c6382e926434694700d1
Antonio Credoz <antonio.credoz@gmail.com>
2017-11-08 15:30:43 -0600

    sending mail

--- server/send_mail.php
+++ server/send_mail.php
@@ -7,41 +7,46 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 //Load composer's autoloader
-require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+require '/var/www/html/vendor/autoload.php';
+
+$mail = new PHPMailer(true);// Passing `true` enables exceptions
+$username='soporte.gif360@gmail.com';
+$pswd=base64_decode('WjJsbU16WXc=');
+$m_attch='/home/onikom/Pictures/celticMark.png';
+$recipient='antonio.credoz@gmail.com';
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPDebug = 2;                                   // Enable verbose debug output
+    $mail->isSMTP();                                        // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'user@example.com';                 // SMTP username
-    $mail->Password = 'secret';                           // SMTP password
+    $mail->Username = $username;                 // SMTP username
+    $mail->Password = $pswd;                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom($username, 'Gif360');
+    //$mail->addAddress('antonio.credoz@gmail.com', 'Anthony');     // Add a recipient
+    $mail->addAddress($recipient);               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment($m_attch, 'wmark.png');    // Optional name
 
     //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Gif360';
+    $mail->Body    = '<b>!</b>Este correo contiene un  <b>Gif360!</b>';
+    $mail->AltBody = '!Este correo contiene un Gif360!';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Message has been sent' . "\r\n";
 } catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    echo 'Message could not be sent.' . "\r\n";
+    echo 'Mailer Error: ' . $mail->ErrorInfo . "\r\n";
 }
